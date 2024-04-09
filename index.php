cf2eefe0cbe8bc168db52f900364cece5cda859c
Tien Han <tienthuyhan@gmail.com>
2024-04-09 13:42:41 -0400

    Add in home page view

--- index.php
+++ index.php
@@ -14,7 +14,11 @@
 
     //Define a default route
     $f3-> route('GET /', function() {
-        echo '<h1>Hello Fat-Free!</h1>';
+        //echo '<h1>Hello Fat-Free!</h1>';
+
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/home.html');
     });
 
     //Run Fat-Free
